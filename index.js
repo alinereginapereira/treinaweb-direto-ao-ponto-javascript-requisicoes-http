a1f184f5c62fd251e43e94ab68bf1569cae3f996
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-03-23 15:34:21 -0300

    01:08 - Response

--- index.js
+++ index.js
@@ -1,100 +1,19 @@
-// izEpTZSPnqntc7WR
-async function getAlunos() {
-    return await fetch("http://localhost:3002/api/alunos")
-        .then((response) => {
-            return response.json();
-        }).then((data) => {
-            console.log(data);
-            return data;
-        });
-}
+// recursos
+// Isto define o recurso que você deseja buscar. Isto pode ser:
+
+// configurações
+// Um objeto contendo quaisquer configurações
+// customizadas que você deseja aplicar à solicitação
+
+// fetch(resource, init);
+
+// BaseUrl = http://localhost:3002
+// Endpoint = /api/alunos
+// Query ou Parans = qualquer coisa após p "?" "/alunos?id=123&nome=ariel"
 
 
-getAlunos();
-
-async function criarAluno() {
-    const aluno = {
-        nome: "Elton Fonseca",
-        idade: 36,
-        sexo: "Masculino",
-        curso: "Gestão TI",
-        notas: [8.0, 7.5, 9.2]
-    }
-
-
-    await fetch("http://localhost:3002/api/alunos", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(aluno)
-    }).then((response) => {
-        console.log(response);
-        return response.json();
-    }).then((data) => {
-        console.log(data);
-        return data;
-    });
-}
-
-
-async function editarAluno() {
-    const aluno = {
-        nome: "Marina Silva",
-        idade: 28,
-        sexo: "Feminino",
-        curso: "Gestão TI",
-        notas: [8.0, 7.5, 9.2],
-        id: "izEpTZSPnqntc7WR"
-    }
-
-
-    fetch("http://localhost:3002/api/alunos?id=izEpTZSPnqntc7WR", {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(aluno),
-    })
-        .then(response => response.json()
-        ).then((data) => {
-            console.log(data);
-        })
-}
-
-
-async function editarValorAluno() {
-    const aluno = {
-        idade: 30,
-    }
-
-    fetch("http://localhost:3002/api/alunos?" + new URLSearchParams({
-        id: "izEpTZSPnqntc7WR",
-        nome: "ariel+sardinha"
-    }), {
-        method: "PATH",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(aluno),
-    })
-        .then(response => response.json()
-        ).then((data) => {
-            console.log(data);
-        })
-}
-
-
-async function deletarAluno() {
-    return fetch("http://localhost:3002/api/alunos?" + new URLSearchParams({
-        id: "izEpTZSPnqntc7WR"
-    }), {
-        method: "DELETE"
-    })
-        .then((response) => response.json)
-        .then((data) => {
-            console.log(data);
-        })
-}
-
-// deletarAluno();
+fetch("http://localhost:3002/api/alunos?", { // recursos
+    method: "GET", // O método da requisição
+    headers: { 'Content-Type': 'application/json' }, // cabeçalho da requisição
+    body: JSON.stringify(novosDadosAluno), // corpo da requisição
+})
