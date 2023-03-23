105d84969d8c95648d85d2433d2e1d87a10ab929
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-03-23 15:33:14 -0300

    01:04 - Salvando dados com POST

--- index.js
+++ index.js
@@ -12,3 +12,32 @@ async function getAlunos() {
 
 
 getAlunos();
+
+async function criarAluno() {
+    const aluno = {
+        nome: "Elton Fonseca",
+        idade: 36,
+        sexo: "Masculino",
+        curso: "Gestão TI",
+        notas: [8.0, 7.5, 9.2]
+    }
+
+
+    await fetch("http://localhost:3002/api/alunos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(aluno)
+    }).then((response) => {
+        console.log(response);
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+        return data;
+    });
+}
+
+// criarAluno();
+
+
